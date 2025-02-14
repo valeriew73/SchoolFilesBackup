0e398957f29875b967ec333749c6fde6d88ab123
valeriew73 <valerievwilson73@gmail.com>
2025-02-14 14:24:09 +0700

    E-Learning Commit

---
+++ JC2/Tasks/Q2P4MJ2022.py
@@ -0,0 +1,12 @@
+import random
+
+#2 a)
+# declaration
+twoDArray = []
+for i in range(10):
+    twoDArray.append([0 for i in range(10)])
+
+# initialisation
+for  row in range(10):
+    for col in range(10):
+        twoDArray[row][col] = random.randint(0, 100)
