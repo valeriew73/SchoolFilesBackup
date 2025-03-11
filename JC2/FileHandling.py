45748b2ffe72c18c731fad43e053003c081afe37
valeriew73 <valerievwilson73@gmail.com>
2025-03-11 13:46:06 +0700

    Commit for file handling

---
+++ JC2/FileHandling.py
@@ -0,0 +1,4 @@
+# If you want to store anything other than text, you need to use a binary file (.dat) (ex. for records)
+# opening is the same, but:
+# instead of readline, use getrecord
+# instead of writeline, use putrecord
