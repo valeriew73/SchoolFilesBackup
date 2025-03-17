d37555de2850c6ffda484e8cf669626b06893c55
valeriew73 <valerievwilson73@gmail.com>
2025-03-17 10:58:46 +0700

    Test

--- JC2/FileHandling.py
+++ JC2/FileHandling.py
@@ -2,3 +2,15 @@
 # opening is the same, but:
 # instead of readline, use getrecord
 # instead of writeline, use putrecord
+
+# You don't have to learn to use binary files in python because it's not in the syllabus and you need the pickle library
+
+import pickle
+# (load and dump)
+
+
+class Studentrecord:
+    def __init__(self, studentID, studentClass):
+        self.studentID = studentID
+        self.studentClass = studentClass
+
