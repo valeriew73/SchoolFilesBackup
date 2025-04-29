187d6326079025f319da15656d3dea114d36942c
valeriew73 <valerievwilson73@gmail.com>
2025-04-29 08:26:38 +0700

    test

---
+++ JC2/test.py
@@ -0,0 +1,19 @@
+integer = 0
+print(f"outside function: {integer}")
+def modify(immutable_var):
+    immutable_var += 2
+    print(f"inside function: {immutable_var}")
+modify(integer)
+print(f"after function, outside function: {integer}")
+
+
+array = []
+print(f"outside function: {array}")
+def modify(immutable_var):
+    immutable_var += 2
+    print(f"inside function: {immutable_var}")
+modify(array)
+print(f"after function, outside function: {array}")
+
+string = "ABCD"
+string[0] = "A"
