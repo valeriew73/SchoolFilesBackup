d709ca40e7b1fa242f247d080c06558aea4fe9a5
valeriew73 <valerievwilson73@gmail.com>
2025-05-02 15:36:19 +0700

    Commit

--- JC2/MiscFunctions.py
+++ JC2/MiscFunctions.py
@@ -7,9 +7,8 @@ print(name.split()) # if a parameter is not provided, then the string will be sp
 comma_name = "Valerie,Wilson"
 print(comma_name.split(","))
 
-# The strip function removes leading and trailing character parameters 
+# The strip function removes leading and trailing character parameters from the string
 # (unordered and can be combined, each character added to the string is treated as one item)
-# from the string
 fruit = "    banana   "
 print(fruit.strip()) # if a parameter is not provided, trailing whitespaces or \n will be removed
 
--- JC2/test2.py
+++ JC2/test2.py
@@ -1,5 +1,5 @@
 modtest = 5 % 2 #1
 divtest = 5 // 2 #2
 
-print("test1")
-print("test2")
+print(modtest)
+print(divtest)
